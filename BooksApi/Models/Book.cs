650a944540cc4f3d8364aca5984aaf051a293da2
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-02-11 01:35:16 +0800

    MongoDB WebAPI

---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Models
+{/// <summary>
+ /// 需要在将公共语言运行时 (CLR) 对象映射到 MongoDB 集合时使用。
+///    使用[BsonId] 进行批注，以将此属性指定为文档的主键。
+///使用[BsonRepresentation(BsonType.ObjectId)] 进行批注，以允许将参数作为类型 string 而非 ObjectId 结构传递。 Mongo 处理从 string 到 ObjectId 的转换。
+///BookName 属性使用[BsonElement] 特性进行批注。 Name 的属性值表示 MongoDB 集合中的属性名称。
+/// </summary>
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
+        public string BookName { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ BooksApi/Services/BookService.cs
@@ -0,0 +1,47 @@
+﻿using BooksApi.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        /// <summary>
+        /// 上面的代码通过构造函数注入从 DI 检索 IBookstoreDatabaseSettings 实例。 使用此方法可访问在添加配置模型部分添加的 appsettings.json 配置值 。
+
+        /// </summary>
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() =>
+            _books.Find(book => true).ToList();
+
+        public Book Get(string id) =>
+            _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create (Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn) =>
+            _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(Book bookIn) =>
+            _books.DeleteOne(book => book.Id == bookIn.Id);
+
+        public void Remove(string id) =>
+            _books.DeleteOne(book => book.Id == id);
+    }
+}
