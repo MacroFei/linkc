1c4195c594833e251db5090d927d1293a4dc3f72
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-02-11 01:53:57 +0800

    JavaScript WebApi

--- JavaScriptWebApi/Startup.cs
+++ JavaScriptWebApi/Startup.cs
@@ -6,14 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using TodoApi.Models;
 
-namespace TodoApi
+namespace JavaScriptWebApi
 {
     public class Startup
     {
@@ -27,8 +25,6 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt =>
-           opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
@@ -39,11 +35,8 @@ namespace TodoApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            //js
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
-
 
             app.UseHttpsRedirection();
 
