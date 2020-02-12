3acc87a9d020ab3e1900d7dfe6fdd649a76bd1ee
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-02-12 23:53:46 +0800

    EF Core 和 Razor Pages

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        /// <summary>
+        ///  使用 ICollection<Enrollment> 时，EF Core 会默认创建 HashSet<Enrollment> 集合。
+        /// </summary>
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
