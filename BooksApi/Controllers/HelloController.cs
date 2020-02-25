1aac0691bd43336e435555b24f3fd46b8d76f15a
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-02-25 16:30:47 +0800

    Blog

---
+++ BooksApi/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+       {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        [HttpGet]
+        public IEnumerable<String> Get()
+        {
+            return Summaries.ToList();
+        } 
+    }
+}
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +9,38 @@ namespace ContosoUniversity.Models
 {
     public class Student
     {
+        //public int ID { get; set; }
+        //public string LastName { get; set; }
+        //public string FirstMidName { get; set; }
+        //public DateTime EnrollmentDate { get; set; }
+        ///// <summary>
+        /////  使用 ICollection<Enrollment> 时，EF Core 会默认创建 HashSet<Enrollment> 集合。
+        ///// </summary>
+        //public ICollection<Enrollment> Enrollments { get; set; }
+
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.)")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
-        /// <summary>
-        ///  使用 ICollection<Enrollment> 时，EF Core 会默认创建 HashSet<Enrollment> 集合。
-        /// </summary>
+        [Display(Name ="Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
